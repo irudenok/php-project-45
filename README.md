6cc9dfccc89f74831644b1ade042a7c5591e015d
Ilia <rudenok.it@gmail.com>
2025-11-13 13:51:15 +0300

    Brain calc

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/irudenok/php-project-45/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/irudenok/php-project-45/actions)
 
-## Brain even?
+## Brain even
 [![asciicast](https://asciinema.org/a/q3IJyZnXbiaICfW7ZljWkrc5r.svg)](https://asciinema.org/a/q3IJyZnXbiaICfW7ZljWkrc5r)
+
+## Brain calc
+[![asciicast](https://asciinema.org/a/DpyvkKggG3VGbwbRassmxUKm4.svg)](https://asciinema.org/a/DpyvkKggG3VGbwbRassmxUKm4)
---
+++ src/Engine.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace BrainGames\Engine;
+
+use function cli\line;
+use function cli\prompt;
+
+function exec($description, $data)
+{
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+
+    line($description);
+
+    foreach ($data as $question => $answer) {
+        line("Question: {$question}");
+        $userAnswer = prompt('Your answer ');
+
+        if ($userAnswer != $answer) {
+            line("'{$userAnswer}' is wrong answer ;(. Correct answer was '{$answer}'.");
+            line("Let's try again, {$name}!");
+            return;
+        }
+        line('Correct!');
+    }
+
+    line("Congratulations, {$name}!");
+}
